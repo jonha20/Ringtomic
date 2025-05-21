617d3380359402dad5d7011a972b7bbd5c7b8516
jonha20 <jonathan812000@gmail.com>
2025-05-22 00:42:16 +0200

    add/registro y login sin jwt

---
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -0,0 +1,90 @@
+import React , { useState , useEffect}  from "react";
+import { Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+const SignUp = () => {
+  const [name, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+
+    useEffect(() => {
+    const emailValidation = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (!emailValidation.test(email) && email.length > 0) {
+      setEmailMessage("Email must have a valid format");
+    } else {
+      setEmailMessage("");
+    }
+  }, [email])
+
+  useEffect(() => {
+    const passwordValidation = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{9,}$/
+    if (!passwordValidation.test(password) && password.length > 0) {
+      setPasswordMessage("Password must contain lowecase, uppercase, digit and special character");
+    } else {
+      setPasswordMessage("");
+    }
+  }, [password])
+
+  const handleSignUp = async (e) => {
+      e.preventDefault();
+    try {
+      const request = await axios({
+        method: 'post',
+        url: 'http://localhost:3000/users/register',
+        data: { name ,email , password }
+      });
+      setMessage(request.data.msg);
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
+  return <>
+
+    <div className="signup">
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSignUp}>
+        <div>
+          <label htmlFor="name">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={name}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          {emailMessage ? <span>{emailMessage}</span> : ""}
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+           {passwordMessage ? <span>{passwordMessage}</span> : ""}
+        </div>
+        <button type="submit">Sign Up</button>
+      </form>
+      <span>{message}</span>
+      <p>
+        Already have an account? <a href="/login">Log in</a>
+      </p>
+    </div>
+
+  </>;
+};
+
+export default SignUp;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,32 @@
 require('dotenv').config();
 const express = require("express"); // Importamos el paquete express
 const app = express(); // Inciializar servidor con express
-const port = 3001; // Puerto a usar por el servidor
+const port = 3000; // Puerto a usar por el servidor
 const morgan = require('morgan');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // Cambia esto a la URL de tu frontend
+  credentials: true // Permite el envío de cookies
+}));
 
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client')));
+console.log(path.join(__dirname, '../client/index.html')) // Imprime la ruta de la carpeta client
 
-
-console.log(`Directorio de la app: ${path.join(__dirname, '../client/build')}`);
 // Handles any requests that don't match the ones above
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
-app.get('/', (req, res) => {
-  res.send('¡Ringtomic!');
-});
+// app.get('/', (req, res) => {
+//   res.send('¡Ringtomic!');
+// });
 
 const userRoutes = require('./routes/user.routes');
 const pitchesRoutes = require('./routes/pitches.routes');
