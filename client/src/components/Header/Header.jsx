476fdc191771eaf8d1fd9b0137b396ede9fad599
jonha20 <64472507+jonha20@users.noreply.github.com>
2025-05-22 14:25:58 +0200

    add/components react y scss

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Nav from "./Nav";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/logo.png";
+//import Menu from react-burger-menu
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <header className="header">
+      <img src={Logo} alt="Logo" className="header__logo" />
+      <div className="header__right">
+        <Nav />
+        <Link to="/logout">
+          <button>Logout</button>
+        </Link>
+      </div>
+    </header>
+    </>
+  );
 };
 
 export default Header;
